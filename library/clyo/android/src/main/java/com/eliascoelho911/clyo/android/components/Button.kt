dba4a31add957cbd1163a5d497bf80145967eb9e
elias <elias_coelho911@hotmail.com>
2022-11-08 19:15:04 -0300

    cria componente de botão

--- library/clyo/android/src/main/java/com/eliascoelho911/clyo/android/components/Button.kt
+++ library/clyo/android/src/main/java/com/eliascoelho911/clyo/android/components/Button.kt
@@ -1,7 +1,9 @@
-package com.eliascoelho911.clyo.core.ui.component
+package com.eliascoelho911.clyo.android.components
 
 import androidx.annotation.StyleRes
 import com.eliascoelho911.clyo.core.ui.action.Action
+import com.eliascoelho911.clyo.core.ui.component.BindProperty
+import com.eliascoelho911.clyo.core.ui.component.Component
 
 data class Button(
     @BindProperty("text")
