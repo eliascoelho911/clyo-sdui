7afa72db20d974eb7ebfaf1c670fa915ed37050f
elias <elias_coelho911@hotmail.com>
2022-11-09 16:49:21 -0300

    altera retorno de property

--- library/clyo/core/kotlin/src/main/java/com/eliascoelho911/clyo/core/data/component/ComponentData.kt
+++ library/clyo/core/kotlin/src/main/java/com/eliascoelho911/clyo/core/data/component/ComponentData.kt
@@ -8,17 +8,16 @@ import kotlin.reflect.full.hasAnnotation
 import kotlinx.serialization.SerialName
 import kotlinx.serialization.Serializable
 import kotlinx.serialization.json.Json
-import kotlinx.serialization.json.JsonObject
+import kotlinx.serialization.json.JsonPrimitive
 import kotlinx.serialization.json.decodeFromJsonElement
 
 @Serializable
 data class ComponentData(
     @SerialName("key") val key: String,
-    @SerialName("properties") val properties: JsonObject
+    @SerialName("properties") val properties: Map<String, JsonPrimitive>
 ) {
     inline fun <reified T : Any> getPropertyByKey(key: String): T {
-        val propertyValue = properties[key]
-        requireNotNull(propertyValue)
+        val propertyValue = properties[key] ?: error("Property with key $key not found")
         return Json.decodeFromJsonElement(propertyValue)
     }
 
