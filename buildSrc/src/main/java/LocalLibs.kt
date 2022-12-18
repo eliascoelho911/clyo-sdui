da4096e6823ec07b1e11c65d2e109dd154564c11
elias <elias_coelho911@hotmail.com>
2022-12-18 16:01:20 -0300

    cria módulo ui

--- buildSrc/src/main/java/LocalLibs.kt
+++ buildSrc/src/main/java/LocalLibs.kt
@@ -1,7 +1,7 @@
 object LocalLibs {
-    const val clyoCore = ":library:clyo:core:core"
-    const val clyoData = ":library:clyo:core:data:contract"
-    const val clyoDataKotlinxSerialization = ":library:clyo:core:data:kotlinx-serialization"
+    const val clyoCore = ":library:clyo:core"
+    const val clyoData = ":library:clyo:data:contract"
+    const val clyoDataKotlinxSerialization = ":library:clyo:data:kotlinx-serialization"
     const val clyoContainers = ":library:clyo:impl:containers"
     const val clyoMaterial3Components = ":library:clyo:impl:material3-components"
 }
