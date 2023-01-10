479d751e619b7c4c4f1f17bdef9b4ff584cbd538
elias <elias_coelho911@hotmail.com>
2023-01-10 19:00:13 -0300

    Testes unitários para ComponentDeclarationDSL

---
+++ library/clyo/android/core/src/test/java/com/clyo/android/module/dsl/ComponentDeclarationDSLTest.kt
@@ -0,0 +1,41 @@
+package com.clyo.android.module.dsl
+
+import android.view.View
+import com.clyo.android.annotation.ClyoInternalAPI
+import com.clyo.android.component.GenericViewBinder
+import com.clyo.android.module.Module
+import com.clyo.data.ComponentName
+import com.clyo.data.PropertiesData
+import io.mockk.Runs
+import io.mockk.every
+import io.mockk.just
+import io.mockk.mockk
+import io.mockk.slot
+import kotlin.test.assertEquals
+import org.junit.Test
+
+internal class ComponentDeclarationDSLTest {
+
+    @OptIn(ClyoInternalAPI::class)
+    @Test
+    fun `binder declare GenericViewBinder`() {
+        // Given
+        val module = mockk<Module>(relaxed = true)
+        val componentName = ComponentName("myComponent")
+        val componentDeclarationDSL = ComponentDeclarationDSL<View>(componentName, module)
+        val block: View.(properties: PropertiesData) -> Unit = {
+            // do nothing
+        }
+        val slot = slot<() -> GenericViewBinder<View>>()
+        val expected = GenericViewBinder(block)
+
+        every { module.declare(componentName, viewBinder = capture(slot)) } just Runs
+
+        // When
+        componentDeclarationDSL.binder(block)
+
+        // Then
+        val result = slot.captured.invoke()
+        assertEquals(expected, result)
+    }
+}
