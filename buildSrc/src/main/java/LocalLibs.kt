e54e99deeb5d496bda6e7d7d2fcb71a99098c748
elias <elias_coelho911@hotmail.com>
2023-03-01 18:53:38 -0300

    Remove modulo common

--- buildSrc/src/main/java/LocalLibs.kt
+++ buildSrc/src/main/java/LocalLibs.kt
@@ -1,5 +1,4 @@
 object LocalLibs {
-    const val clyoCommon = ":library:clyo:common"
     const val clyoAndroid = ":library:clyo:android:core"
     const val clyoContainers = ":library:clyo:android:containers"
     const val clyoMaterial3 = ":library:clyo:android:widgets:material3"
