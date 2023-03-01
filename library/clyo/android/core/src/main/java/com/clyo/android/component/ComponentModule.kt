1c4ef31dcdda857c9345cfdc88a72137f981bd5a
elias <elias_coelho911@hotmail.com>
2023-03-01 19:35:13 -0300

    Refatora module

--- library/clyo/android/core/src/main/java/com/clyo/android/component/ComponentModule.kt
+++ library/clyo/android/core/src/main/java/com/clyo/android/component/ComponentModule.kt
@@ -4,15 +4,62 @@ import android.view.View
 import kotlin.reflect.KClass
 
 interface ComponentModule {
-    fun putComponentKClass(name: ComponentName, kClass: KClass<out View>)
+    fun putViewKClass(name: ComponentName, kClass: KClass<out View>)
 
     fun putComponentBinder(name: ComponentName, binder: () -> ComponentBinder<*>)
 
     fun putAll(module: ComponentModule)
 
-    fun getComponentKClassOrNull(name: ComponentName): KClass<out View>
+    fun getViewKClassOrNull(name: ComponentName): KClass<out View>?
 
-    fun getComponentBinder(name: ComponentName): ComponentBinder<*>
+    fun getAllViewKClasses(): Map<ComponentName, KClass<out View>>
+
+    fun getComponentBinderOrNull(name: ComponentName): ComponentBinder<*>?
+
+    fun getAllComponentBinders(): Map<ComponentName, () -> ComponentBinder<*>>
 
     fun clear()
 }
+
+internal class ComponentModuleImpl : ComponentModule {
+
+    private val viewKClassesMap = HashMap<ComponentName, KClass<out View>>()
+
+    private val componentBindersMap = HashMap<ComponentName, () -> ComponentBinder<*>>()
+
+    override fun putViewKClass(name: ComponentName, kClass: KClass<out View>) {
+        viewKClassesMap[name] = kClass
+    }
+
+    override fun putComponentBinder(name: ComponentName, binder: () -> ComponentBinder<*>) {
+        componentBindersMap[name] = binder
+    }
+
+    override fun putAll(module: ComponentModule) {
+        viewKClassesMap.putAll(module.getAllViewKClasses())
+        componentBindersMap.putAll(module.getAllComponentBinders())
+    }
+
+    override fun getViewKClassOrNull(name: ComponentName): KClass<out View>? {
+        return viewKClassesMap[name]
+    }
+
+    override fun getAllViewKClasses(): Map<ComponentName, KClass<out View>> {
+        return viewKClassesMap
+    }
+
+    override fun getComponentBinderOrNull(name: ComponentName): ComponentBinder<*>? {
+        return componentBindersMap[name]?.invoke()
+    }
+
+    override fun getAllComponentBinders(): Map<ComponentName, () -> ComponentBinder<*>> {
+        return componentBindersMap
+    }
+
+    override fun clear() {
+        componentBindersMap.clear()
+        viewKClassesMap.clear()
+    }
+}
+
+fun emptyModule(): ComponentModule = ComponentModuleImpl()
--- library/clyo/android/core/src/main/java/com/clyo/android/tools/ClyoExternalTools.kt
+++
@@ -1,7 +0,0 @@
-package com.clyo.android.tools
-
-internal object ClyoExternalTools {
-    fun <K, V> safeMutableMapOf(initialContent: Map<K, V> = emptyMap()): MutableMap<K, V> =
-        HashMap(initialContent)
-}
-
