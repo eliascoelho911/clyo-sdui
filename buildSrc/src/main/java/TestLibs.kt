30b43e98c99983e28f6c701e4c827e99c7babcde
elias <elias_coelho911@hotmail.com>
2023-07-07 13:35:33 -0300

    Create data module

--- buildSrc/src/main/java/TestLibs.kt
+++ buildSrc/src/main/java/TestLibs.kt
@@ -2,5 +2,6 @@ object TestLibs {
     // JUnit
     const val junit = "junit:junit:${Versions.junit}"
     const val kotlinTestJunit = "org.jetbrains.kotlin:kotlin-test-junit:${Versions.kotlin}"
-    const val mockk = "io.mockk:mockk-android:${Versions.mockk}"
+    const val mockk = "io.mockk:mockk:${Versions.mockk}"
+    const val mockkAndroid = "io.mockk:mockk-android:${Versions.mockk}"
 }
