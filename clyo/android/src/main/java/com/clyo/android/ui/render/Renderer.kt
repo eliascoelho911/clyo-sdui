6744807b0063d72f9ddf3e1f67dd07d138889e71
elias <elias_coelho911@hotmail.com>
2023-07-10 12:48:32 -0300

    Move container renderer to another file

--- clyo/android/src/main/java/com/clyo/android/ui/render/Renderer.kt
+++ clyo/android/src/main/java/com/clyo/android/ui/render/Renderer.kt
@@ -5,11 +5,9 @@ import com.clyo.android.ui.component.container.Container
 import com.clyo.data.properties.PropertiesJson
 import kotlinx.serialization.json.Json
 
-internal class Renderer(
-    private val json: Json,
-    private val container: Container<*>
-) {
-    fun render(propertiesJson: PropertiesJson) {
+internal class Renderer(private val json: Json) {
+
+    fun render(container: Container<*>, propertiesJson: PropertiesJson) {
         container.children.forEach { widget ->
             widget.render(propertiesJson.decodeProperties(json, widget.id))
         }
