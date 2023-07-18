9a7f348129e03bc9ddc9ce88d0e0f815c89390d7
elias <elias_coelho911@hotmail.com>
2023-07-18 20:11:06 -0300

    Container test

---
+++ clyo/android/src/test/java/com/clyo/android/ui/page/PageTest.kt
@@ -0,0 +1,38 @@
+package com.clyo.android.ui.page
+
+import android.view.ViewGroup
+import com.clyo.android.ui.component.container.Container
+import com.clyo.data.properties.Properties
+import io.mockk.mockk
+import io.mockk.verify
+import org.junit.Test
+
+internal class PageTest {
+    private val container = mockk<Container<*>>(relaxed = true)
+    private val page = Page(container)
+
+    @Test
+    fun `should attach container to parent`() {
+        // given
+        val parent = mockk<ViewGroup>(relaxed = true)
+
+        // when
+        page.attachTo(parent)
+
+        // then
+        verify { parent.removeAllViews() }
+        verify { parent.addView(container.view) }
+    }
+
+    @Test
+    fun `should render container content`() {
+        // Given
+        val propertiesCreator = mockk<(widgetId: String) -> Properties?>()
+
+        // When
+        page.render(propertiesCreator)
+
+        // Then
+        verify { container.renderContent(propertiesCreator) }
+    }
+}
