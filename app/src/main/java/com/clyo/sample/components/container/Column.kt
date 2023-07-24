4522dc79a9bc4f6f15835c186b7ff27e6d6cf4d3
elias <elias_coelho911@hotmail.com>
2023-07-24 19:17:54 -0300

    commit

--- app/src/main/java/com/clyo/sample/components/container/Column.kt
+++
@@ -1,8 +0,0 @@
-package com.clyo.sample.components.container
-
-import android.content.Context
-import com.clyo.android.component.Container
-
-internal class Column(context: Context) : Container<ColumnLayout>() {
-    override val view = ColumnLayout(context)
-}
--- app/src/main/java/com/clyo/sample/presentation/ClyoSampleApplication.kt
+++ app/src/main/java/com/clyo/sample/presentation/ClyoSampleApplication.kt
@@ -1,10 +1,6 @@
 package com.clyo.sample.presentation
 
 import android.app.Application
-import com.clyo.android.startClyo
-import com.clyo.sample.impl.ClyoSampleJson
-import com.clyo.sample.impl.container.ContainerProviderImpl
-import com.clyo.sample.impl.widget.WidgetProviderImpl
 import org.koin.android.ext.koin.androidContext
 import org.koin.core.context.startKoin
 
@@ -14,12 +10,6 @@ internal class ClyoSampleApplication : Application() {
 
         startKoin {
             androidContext(this@ClyoSampleApplication)
-        }
-
-        startClyo {
-            containerProvider = ContainerProviderImpl()
-            widgetProvider = WidgetProviderImpl()
-            json = ClyoSampleJson
         }
     }
 }
